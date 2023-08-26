20d37aaf5ac56234e347afffe53d9559609c23fc
hb <>
2023-08-26 04:12:08 -0700

    switch to material icons

---
+++ .config/icons.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Icons
+
+export ICON_CMD=󰘳
+export ICON_SETTINGS=󰒓
+export ICON_TASKMANAGER=󱕍
+export ICON_SLEEP=󰌾
+
+export ICON_SPACE=󱂬
+export ICON_DEV=󰅨
+export ICON_WEB=󰇧
+export ICON_CHAT=󰍨
+export ICON_MAIL=󰇮
+export ICON_CAL=󰸗 # App
+export ICON_MUSIC=󰀥
+
+export ICON_CLOCK=󰥔
+export ICON_CALENDAR=󰃶 # Status
+
+export ICON_WIFI=󰖩
+export ICON_WIFI_OFF=󰖪
+export ICON_VPN=󰦝
+
+export ICONS_VOLUME=(󰸈 󰕿 󰖀 󰕾)
+
+export ICONS_BATTERY=(󰂎 󰁺 󰁻 󰁼 󰁽 󰁾 󰁿 󰂀 󰂁 󰂂 󰁹)
+export ICONS_BATTERY_CHARGING=(󰢟 󰢜 󰂆 󰂇 󰂈 󰢝 󰂉 󰢞 󰂊 󰂋 󰂅)
+
+export ICON_SWAP=󰁯
+export ICON_RAM=󰓅
+export ICON_DISK=󰋊
+export ICON_CPU=󰘚
